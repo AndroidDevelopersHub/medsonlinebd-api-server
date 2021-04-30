473210bcf94fbee976ba0a069604b7ee051ffd6b
Tonmoy ParallaxLogic <tomnoy@parallaxlogic.net>
2021-04-30 17:08:08 +0600

    request

---
+++ server/api/request.js
@@ -0,0 +1,220 @@
+const express = require("express");
+const db = require("./db");
+const router = express.Router();
+let jwt = require("jsonwebtoken");
+const config = require("../../middleware/config.json"); // refresh
+let tokenChecker = require("../../middleware/tockenchecker");
+const tokenList = {};
+const _response = require('../common/middleware/api-response')
+const responsemsg = require('../common/middleware/response-msg')
+const responsecode = require('../common/middleware/response-code')
+const response = require('../common/middleware/api-response')
+const Joi = require('@hapi/joi')
+const bcrypt = require('bcrypt');
+const commonServe = require('../common/services/commonServices')
+
+
+module.exports = function (router) {
+    router.get('/request', list);
+    router.post('/request', add);
+    router.put('/request/:id', update);
+    router.get('/request/:id', details);
+    router.delete('/request/:id', _delete);
+
+}
+
+
+const schema = Joi.object({
+    medicine_name: Joi.string().min(3).required(),
+    latlang: Joi.string().required(),
+    //token: Joi.string().required()
+});
+
+function add(req, res){
+    //
+    var medicine_name = req.body.medicine_name;
+    var latlang = req.body.latlang;
+    const { error } = schema.validate(req.body);
+    if (error) return _response.apiFailed(res ,error.details[0].message)
+
+    db.query("INSERT INTO request SET ?", req.body , (err, result) => {
+        if (!err) {
+            return _response.apiSuccess(res, responsemsg.userSaveSuccess , result)
+        } else {
+            return _response.apiFailed(res, err , result)
+        }
+    });
+
+
+}
+
+async function list(req ,res ){
+
+    var limit = 500;
+    var page = 1;
+    var totalDocs = 0;
+    if (req.query.page){
+        page = req.query.page
+    }
+    if (req.query.limit){
+        limit = req.query.limit
+    }
+    var offset = (page - 1) * limit
+
+
+    db.query("SELECT COUNT(*) AS total FROM request", (err, result) => {
+        if (!err) {
+            totalDocs = result[0].total
+        } else {
+
+        }
+    });
+
+
+
+    //Search by String
+    if (req.query.search_string && req.query.search_string !== ''){
+
+        db.query("SELECT * FROM request WHERE CONCAT(username, email,phone_number) REGEXP '"+req.query.search_string+"'  LIMIT "+limit+" OFFSET "+offset+" ", (err, result) => {
+            if (!err && result.length > 0) {
+                return _response.apiSuccess(res, result.length+" "+responsemsg.redeemFound , result,{page: parseInt(page) , limit: parseInt(limit),totalDocs: totalDocs })
+
+            } else {
+                return _response.apiFailed(res, responsemsg.userListIsEmpty)
+            }
+        });
+
+
+    }else {
+        db.query("SELECT * FROM request LIMIT "+limit+" OFFSET "+offset+" ", (err, result) => {
+            if (!err) {
+                return _response.apiSuccess(res, result.length+" "+responsemsg.userFound , result , {page: parseInt(page) , limit: parseInt(limit),totalDocs: totalDocs })
+
+            } else {
+                return _response.apiFailed(res, responsemsg.userListIsEmpty )
+            }
+        });
+    }
+
+
+}
+
+function update(req ,res ){
+    var formData = []
+
+    if (req.params.id){
+        db.query("SELECT * FROM `request` WHERE id='"+req.params.id+"'", (err, result) => {
+            if (!err && result.length > 0) {
+
+                db.query("UPDATE request SET ? WHERE id = '"+req.params.id+"'" , req.body ,(err , result) =>{
+                    if (!err){
+                        return _response.apiSuccess(res, responsemsg.userUpdateSuccess)
+                    }else{
+                        return _response.apiFailed(res, err)
+                    }
+                })
+
+            } else {
+                return _response.apiFailed(res, err)
+            }
+        });
+
+    }else {
+        return  _response.apiWarning(res, 'Please select id.')
+
+    }
+}
+
+function details(req ,res ){
+    //const result = bcrypt.compareSync('123', hash);
+    if (req.params.id){
+        db.query("SELECT * FROM `request` WHERE id='"+req.params.id+"'", (err, result) => {
+            if (!err && result.length > 0) {
+                return _response.apiSuccess(res, result.length+" "+responsemsg.userFound ,result)
+            } else {
+                return _response.apiWarning(res , responsemsg.userListIsEmpty)
+            }
+        });
+    }else {
+        return _response.apiWarning(res , 'Please select id')
+    }
+}
+
+function _delete(req ,res){
+
+    if (req.params.id){
+        db.query("SELECT * FROM `request` WHERE id='"+req.params.id+"'", (err, result) => {
+            if (!result.length){
+                return _response.apiWarning(res, responsemsg.userListIsEmpty)
+            }else {
+                db.query("DELETE FROM `request` WHERE id='" + req.params.id + "'", (err, result) => {
+                    if (!err) {
+                        return _response.apiSuccess(res, responsemsg.userDeleteSuccess)
+                    } else {
+                        return _response.apiFailed(res, err)
+                    }
+                });
+            }
+
+        });
+    }else {
+        return _response.apiWarning(res , 'Please select id')
+    }
+}
+
+function walletUpdate(req , res){
+
+    const schema = Joi.object({
+        wallet: Joi.string().required(),
+        increment: Joi.boolean().required()
+    });
+    const { error } = schema.validate(req.query);
+    if (error) return _response.apiFailed(res ,error.details[0].message)
+
+    var response = []
+
+    if (req.params.id){
+        db.query("SELECT * FROM `request` WHERE id='"+req.params.id+"'", (err, result) => {
+
+            if (!err) {
+                response = result[0].wallet
+                console.log(response)
+                if (req.query.increment && req.query.wallet){
+                    if (req.query.increment === 'true'){
+                        var bal = parseFloat(req.query.wallet) + parseFloat(response); // Increment balance
+                        console.log(bal)
+                        db.query("UPDATE request SET wallet ='"+bal+"' WHERE id = '"+req.params.id+"'" , (err , result) =>{
+                            if (!err){
+                                return _response.apiSuccess(res, responsemsg.userWalletUpdateSuccess)
+                            }else{
+                                return _response.apiFailed(res, err)
+                            }
+                        })
+
+                    }else if (req.query.increment === 'false'){
+                        var finalBal = null;
+                        var replaceBal = parseFloat(response) - parseFloat(req.query.wallet) ; // Decrement balance
+                        if (replaceBal > 0){
+                            db.query("UPDATE request SET wallet ='"+replaceBal+"' WHERE id = '"+req.params.id+"'" , (err , result) =>{
+                                if (!err){
+                                    return _response.apiSuccess(res, responsemsg.userWalletUpdateSuccess)
+                                }else{
+                                    return _response.apiFailed(res, err)
+                                }
+                            })
+                        }else {
+                            return _response.apiFailed(res, "This value is big from current balance")
+                        }
+
+                    }
+                }
+
+            } else {
+
+            }
+        });
+    }else {
+        return _response.apiWarning(res , 'Please select id')
+    }
+}
+
